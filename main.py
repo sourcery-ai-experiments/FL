e4621a28879a62b5472312b0b2994a2eec09c5b9
Aren Elchinyan <a2.dev@yandex.ru>
2024-04-15 15:03:34 +0300

    Optimisation

--- main.py
+++ main.py
@@ -6,7 +6,26 @@ app = Flask(__name__)
 
 @app.route("/")
 def tested_fog():
-    return "THE FOG IS COMING" * int(time()/100000)
+    return """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>THE FOG IS COMING</title>
+</head>
+<body>
+    <h1 id="fogText"></h1>
+
+    <script>
+        var fogCount = Math.floor(Date.now() / 10000000);
+        var fogString = "THE FOG IS COMING";
+        for (var i = 0; i < fogCount; i++) {
+            fogString += " THE FOG IS COMING";
+        }
+        document.getElementById("fogText").innerText = fogString;
+    </script>
+</body>
+</html> 
+    """
 
 if __name__ == "__main__":
     app.run(port=9014)
